5306807e4f2b06ef2f4797c5858f840e89044fdf
Alvin Ng <alvindng@gmail.com>
2016-06-03 12:25:40 -0700

    add background image and city output

--- js/scripts.js
+++ js/scripts.js
@@ -10,27 +10,32 @@ $('form').submit(function(event){
     var result = q1 + q2 + q3 + q4;
 
     if (result >= -8 && result <= -4){
+      $(".destination").text("Brazil")
       $(".name").text(nameInput);
       $("#results").show();
       $(".location").text("Brazil!");
 
     }
     else if (result > -4 && result < 0){
+      $(".destination").text("South Korea")
       $(".name").text(nameInput);
       $("#results").show();
       $(".location").text("South Korea!");
     }
     else if (result === 0){
+      $(".destination").text("Chicago")
       $(".name").text(nameInput);
       $("#results").show();
       $(".location").text("Chicago!");
     }
     else if (result >0 && result <= 4){
+      $(".destination").text("Barcelona")
       $(".name").text(nameInput);
       $("#results").show();
       $(".location").text("Barcelona!");
     }
     else if (result >4 && result <= 8) {
+      $(".destination").text("Hong Kong")
       $(".name").text(nameInput);
       $("#results").show();
       $(".location").text("Hong Kong!");
