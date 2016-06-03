0dadc8c1b4d6c7a99b3a9d8f569d9c6da15d0bb1
Alvin Ng <alvindng@gmail.com>
2016-06-03 12:06:01 -0700

    add jquery logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,38 @@
-$(display).ready(function() {
-
-  $(form).submit(function(event){
+$('form').submit(function(event){
     event.preventDefault();
-    var name = ("#myModal1").val();
-    var q1 = parseInt(("#myModal2").val());
-    var q2 = parseInt(("#myModal3").val());
-    var q3 = parseInt(("#myModal4").val());
-    var q4 = parseInt(("#myModal5").val());
-    var q5 = $("input:radio[name=passport]:checked").val();
-    var result = q1 + q2 + q3 + q4 +q5;
 
-    if (result ===0) {
+    var nameInput = $("input#name").val();
+    var q1 = parseInt($("#cost").val());
+    var q2 = parseInt($("#safety").val());
+    var q3 = parseInt($("#food").val());
+    var q4 = parseInt($("#activities").val());
+    var q5 = $("input:radio[name=passport]:checked").val();
+    var result = q1 + q2 + q3 + q4;
+
+    if (result >= -8 && result <= -4){
+      $(".name").text(nameInput);
+      $("#results").show();
+      $(".location").text("Brazil!");
 
     }
+    else if (result > -4 && result < 0){
+      $(".name").text(nameInput);
+      $("#results").show();
+      $(".location").text("South Korea!");
+    }
+    else if (result === 0){
+      $(".name").text(nameInput);
+      $("#results").show();
+      $(".location").text("Chicago!");
+    }
+    else if (result >0 && result <= 4){
+      $(".name").text(nameInput);
+      $("#results").show();
+      $(".location").text("Barcelona!");
+    }
+    else if (result >4 && result <= 8) {
+      $(".name").text(nameInput);
+      $("#results").show();
+      $(".location").text("Hong Kong!");
+    }
   });
-});
