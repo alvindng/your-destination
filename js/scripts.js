0c82dd06bc5074a183b6986dc9a79b3341d7ef0f
Alvin Ng <alvindng@gmail.com>
2016-06-03 14:58:27 -0700

    refine logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,57 @@
 $('form').submit(function(event){
-    event.preventDefault();
+  event.preventDefault();
 
-    var nameInput = $("input#name").val();
-    var q1 = parseInt($("#cost").val());
-    var q2 = parseInt($("#safety").val());
-    var q3 = parseInt($("#food").val());
-    var q4 = parseInt($("#activities").val());
-    var q5 = $("input:radio[name=passport]:checked").val();
-    var result = q1 + q2 + q3 + q4;
+  var nameInput = $("input#name").val();
+  var q1 = parseInt($("#cost").val());
+  var q2 = parseInt($("#safety").val());
+  var q3 = parseInt($("#food").val());
+  var q4 = parseInt($("#activities").val());
+  var q5 = $("input:radio[name=passport]:checked").val();
+  var result = q1 + q2 + q3 + q4;
 
-    if (result >= -8 && result <= -4){
-      $(".destination").text("Paris");
-      $(".name").text(nameInput);
-      $("#results").show();
-      $(".location").text("Paris!");
-      $("#paris").show();
-    }
+  if (result >= -8 && result <= -4){
+    $("img").hide();
+    $(".destination").text("Paris");
+    $(".name").text(nameInput);
+    $("#results").fadeIn();
+    $(".location").text("Paris!");
+    $("#paris").fadeIn();
+  }
 
-    else if (result > -4 && result < 0){
-      $(".destination").text("Brazil");
-      $(".name").text(nameInput);
-      $("#results").show();
-      $(".location").text("Brazil!");
-      $("#brazil").show();
-    }
+  else if (result > -4 && result < 0){
+    $("img").hide();
+    $(".destination").text("Brazil");
+    $(".name").text(nameInput);
+    $("#results").fadeIn();
+    $(".location").text("Brazil!");
+    $("#brazil").fadeIn();
+  }
 
-    else if (result === 0){
-      $(".destination").text("Austria");
-      $(".name").text(nameInput);
-      $("#results").show();
-      $(".location").text("Austria!");
-      $("#austria").show();
-    }
+  else if (result === 0){
+    $("img").hide();
+    $(".destination").text("Austria");
+    $(".name").text(nameInput);
+    $("#results").fadeIn();
+    $(".location").text("Austria!");
+    $("#austria").fadeIn();
+  }
 
-    else if (result >0 && result <= 4){
-      $(".destination").text("Barcelona");
-      $(".name").text(nameInput);
-      $("#results").show();
-      $(".location").text("Barcelona!");
-      $("#barcelona").show();
-    }
+  else if (result >0 && result <= 4){
+    $("img").hide();
+    $(".destination").text("Barcelona");
+    $(".name").text(nameInput);
+    $("#results").fadeIn();
+    $(".location").text("Barcelona!");
+    $("#barcelona").fadeIn();
+  }
 
-    else if (result >4 && result <= 8) {
-      $(".destination").text("Tokyo");
-      $(".name").text(nameInput);
-      $("#results").show();
-      $(".location").text("Tokyo!");
-      $("#tokyo").show();
-    }
-    // $("modal").hide();
+  else if (result >4 && result <= 8) {
+    $("img").hide();
+    $(".destination").text("Tokyo");
+    $(".name").text(nameInput);
+    $("#results").fadeIn();
+    $(".location").text("Tokyo!");
+    $("#tokyo").fadeIn();
+  }
 
-  });
+});
